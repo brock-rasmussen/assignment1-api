d2db02b1d5404534031138f8d1b4460e5105462d
brock-rasmussen <rasmussen.brock@gmail.com>
2016-02-17 20:59:28 -0700

    Add Jade

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
 'use strict';
 
+var path = require('path');
 var express = require('express');
-var app = express();
 
 var reps = require('./lib/reps');
 
-app.get('/all/by-zip/:zip', function (req, res) {
-  reps.allByZip(req.params.zip, function (err, people) {
-    if (err) { return next(err); }
-    res.json(people);
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('view cache', false);
+
+app.get('/', function (req, res, next) {
+  var method;
+  switch (req.query.type) {
+  case 'zip':
+    method = reps.allByZip;
+    break;
+  case 'name':
+    method = reps.repsByName;
+    break;
+  case 'state':
+    method = reps.repsByState;
+    break;
+  }
+      
+  if (method) {
+    method(req.query.search, function (err, people) {
+      if (err) { return next(err); }
+      res.render('index', {
+        reps: people
+      });
+    });
+  }
+  
+  res.render('index', {
+    foo: 'bar',
+    arr: [1, 2, 3, 4, 5, 6, 7]
   });
 });
 
-app.get('/reps/by-name/:name', function (req, res) {
-  reps.repsByName(req.params.name, function (err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
+app.listen(PORT, function () {
+  console.log(`Server started on port ${PORT}`);
 });
-app.get('/reps/by-state/:state', function (req, res) {
-  reps.repsByState(req.params.state, function (err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
-});
-
-app.get('/sens/by-name/:name', function (req, res) {
-  reps.sensByName(req.params.name, function (err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
-});
-app.get('/sens/by-state/:state', function (req, res) {
-  reps.sensByState(req.params.state, function (err, people) {
-    if (err) { return next(err); }
-    res.json(people);
-  });
-});
-
-app.listen(3000);
 
 module.exports = app;
