2285f683c43aeeb5145b0da20689b6b60eebd17a
brock-rasmussen <brock-rasmussen@users.noreply.github.com>
2016-02-18 07:36:30 -0700

    2/18

--- app.js
+++ app.js
@@ -12,6 +12,39 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('view cache', false);
 
+app.get('/all/by-zip/:zip', function (req, res) {
+  reps.allByZip(req.params.zip, function (err, results) {
+    if (err) { return next(err); }
+    res.json(results);
+  });
+});
+
+app.get('/reps/by-name/:name', function (req, res) {
+  reps.repsByName(req.params.name, function (err, results) {
+    if (err) { return next(err); }
+    res.json(results);
+  });
+});
+app.get('/reps/by-state/:state', function (req, res) {
+  reps.repsByState(req.params.state, function (err, results) {
+    if (err) { return next(err); }
+    res.json(results);
+  });
+});
+
+app.get('/sens/by-name/:name', function (req, res) {
+  reps.sensByName(req.params.name, function (err, results) {
+    if (err) { return next(err); }
+    res.json(results);
+  });
+});
+app.get('/sens/by-state/:state', function (req, res) {
+  reps.sensByState(req.params.state, function (err, results) {
+    if (err) { return next(err); }
+    res.json(results);
+	});
+});
+
 app.get('/', function (req, res, next) {
   var method;
   switch (req.query.type) {
@@ -27,18 +60,13 @@ app.get('/', function (req, res, next) {
   }
       
   if (method) {
-    method(req.query.search, function (err, people) {
+    method(req.query.search, function (err, results) {
       if (err) { return next(err); }
       res.render('index', {
-        reps: people
+        reps: results
       });
     });
   }
-  
-  res.render('index', {
-    foo: 'bar',
-    arr: [1, 2, 3, 4, 5, 6, 7]
-  });
 });
 
 app.listen(PORT, function () {
